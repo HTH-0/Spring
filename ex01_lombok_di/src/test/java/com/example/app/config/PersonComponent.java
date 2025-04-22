ba9dd130749c407bc1e479e64036ea58c3eadd0b
HTH-0 <xogud1793@naver.com>
2025-04-22 11:54:01 +0900

    0422

---
+++ ex01_lombok_di/src/test/java/com/example/app/config/PersonComponent.java
@@ -0,0 +1,19 @@
+package com.example.app.config;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@Data
+public class PersonComponent {
+	private String username;
+	private int age;
+	private String addr;
+	
+	PersonComponent(){
+		this.username ="";
+		this.age = 33;
+		this.addr = "인천";
+	}
+}
